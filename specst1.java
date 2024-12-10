326f43c3297b25a039ae4211a5eea26bc4d85a07
jeev86399 <nnani86399@gmail.com>
2024-12-10 15:33:20 +0530

    Add files via upload

---
+++ specst1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class evenst1{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        String ans=" ";
+        for(int i=0;i<a.length();i+=2){
+            ans+=a.charAt(i);
+        }
+        System.out.println(ans);
+    }
+}
